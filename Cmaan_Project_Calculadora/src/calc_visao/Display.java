2978c0ead0e6db1f2376243404976f968c129e09
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-26 08:43:23 -0300

    Update

--- Cmaan_Project_Calculadora/src/calc_visao/Display.java
+++ Cmaan_Project_Calculadora/src/calc_visao/Display.java
@@ -1,9 +1,12 @@
 package calc_visao;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
+	
+	//private JLabel 
 
 	public Display () {
 
