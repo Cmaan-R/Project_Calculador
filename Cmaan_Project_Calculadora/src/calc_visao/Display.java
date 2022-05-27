e1612accd7ec493b6a6ab93b4f6e72c36f53de07
E-134 <E-134@DESKTOP-HC13ERC>
2022-05-27 06:19:37 -0300

    Update

--- Cmaan_Project_Calculadora/src/calc_visao/Display.java
+++ Cmaan_Project_Calculadora/src/calc_visao/Display.java
@@ -1,14 +1,26 @@
 package calc_visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 	
-	//private JLabel 
+	private JLabel label = new JLabel();
+	
 
 	public Display () {
-
-	}
+		setBackground(new Color(46 , 49, 50));
+		label = new JLabel("1234,56");
+		label.setBackground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10 , 25));
+		
+		add(label);
+		}
 }
