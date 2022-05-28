6057b70ec40e3577beb0af72d7650bd37a0a0f42
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-28 15:58:30 -0300

    Update

--- Cmaan_Project_Calculadora/src/calc_visao/Teclado.java
+++ Cmaan_Project_Calculadora/src/calc_visao/Teclado.java
@@ -13,35 +13,6 @@ public class Teclado extends JPanel {
 	private final Color COR_LARANJA = new Color(242, 163, 60);
 	
 	public Teclado () {
-		
-		setLayout(new GridLayout(5 , 4));
-
-		add(new Botao("AC", COR_CINZA_ESCURO));
-		add(new Botao("+/-", COR_CINZA_ESCURO));
-		add(new Botao("%", COR_CINZA_ESCURO));
-		add(new Botao("/" , COR_LARANJA));
-		
-		add(new Botao("7", COR_CINZA_CLARO));
-		add(new Botao("8", COR_CINZA_CLARO));
-		add(new Botao("9", COR_CINZA_CLARO));
-		add(new Botao("*", COR_LARANJA));
-		
-		add(new Botao("7", COR_CINZA_CLARO));
-		add(new Botao("8", COR_CINZA_CLARO));
-		add(new Botao("9", COR_CINZA_CLARO));
-		add(new Botao("*", COR_LARANJA));
-		
-		add(new Botao("7", COR_CINZA_CLARO));
-		add(new Botao("8", COR_CINZA_CLARO));
-		add(new Botao("9", COR_CINZA_CLARO));
-		add(new Botao("*", COR_LARANJA));
-		
-		add(new Botao("7", COR_CINZA_CLARO));
-		add(new Botao("8", COR_CINZA_CLARO));
-		add(new Botao("9", COR_CINZA_CLARO));
-		add(new Botao("*", COR_LARANJA));
-		
-		
-		
+				
 	}
 }
